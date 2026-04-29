OpenTTD/OpenTTD

Codechange: typedef for EnumClassIndexContainer with std::array

src/company_cmd.cpp
@@ -470,7 +470,7 @@ bad_town_name:;
 }
 
 /** Sorting weights for the company colours. */
-static const EnumClassIndexContainer<std::array<uint8_t, to_underlying(Colours::End)>, Colours> _colour_sort{2, 2, 3, 2, 3, 2, 3, 2, 3, 2, 2, 2, 3, 1, 1, 1};
+static const EnumClassArray<uint8_t, Colours, Colours::End> _colour_sort{2, 2, 3, 2, 3, 2, 3, 2, 3, 2, 2, 2, 3, 1, 1, 1};
 /** Similar colours, so we can try to prevent same coloured companies. */
 static const std::initializer_list<Colours> _similar_colour[to_underlying(Colours::End)] = {
 	{Colours::Blue, Colours::LightBlue }, // Colours::DarkBlue

src/fontcache.h
@@ -22,7 +22,7 @@ static const GlyphID SPRITE_GLYPH = 1U << 30;
 /** Font cache for basic fonts. */
 class FontCache {
 protected:
-	static EnumClassIndexContainer<std::array<std::unique_ptr<FontCache>, to_underlying(FontSize::End)>, FontSize> caches; ///< All the font caches.
+	static EnumClassArray<std::unique_ptr<FontCache>, FontSize, FontSize::End> caches; ///< All the font caches.
 	std::unique_ptr<FontCache> parent; ///< The parent of this font cache.
 	const FontSize fs; ///< The size of the font.
 	int height = 0; ///< The height of the font.
@@ -42,9 +42,9 @@ public:
 	static void ClearFontCaches(FontSizes fontsizes);
 
 	/** Default unscaled font heights. */
-	static const EnumClassIndexContainer<std::array<int, to_underlying(FontSize::End)>, FontSize> DEFAULT_FONT_HEIGHT;
+	static const EnumClassArray<int, FontSize, FontSize::End> DEFAULT_FONT_HEIGHT;
 	/** Default unscaled font ascenders. */
-	static const EnumClassIndexContainer<std::array<int, to_underlying(FontSize::End)>, FontSize> DEFAULT_FONT_ASCENDER;
+	static const EnumClassArray<int, FontSize, FontSize::End> DEFAULT_FONT_ASCENDER;
 
 	static int GetDefaultFontHeight(FontSize fs);
 

src/fontcache/spritefontcache.cpp
@@ -33,7 +33,7 @@ static int ScaleFontTrad(int value)
 	return UnScaleByZoom(value * ZOOM_BASE, _font_zoom);
 }
 
-static EnumClassIndexContainer<std::array<std::unordered_map<char32_t, SpriteID>, to_underlying(FontSize::End)>, FontSize> _char_maps{}; ///< Glyph map for each font size.
+static EnumClassArray<std::unordered_map<char32_t, SpriteID>, FontSize, FontSize::End> _char_maps{}; ///< Glyph map for each font size.
 
 /**
  * Get SpriteID associated with a character.

src/rail.h
@@ -262,12 +262,12 @@ public:
 	/**
 	 * NewGRF providing the Action3 for the railtype. nullptr if not available.
 	 */
-	EnumClassIndexContainer<std::array<const GRFFile *, to_underlying(RailSpriteType::End)>, RailSpriteType> grffile{};
+	EnumClassArray<const GRFFile *, RailSpriteType, RailSpriteType::End> grffile{};
 
 	/**
 	 * Sprite groups for resolving sprites
 	 */
-	EnumClassIndexContainer<std::array<const SpriteGroup *, to_underlying(RailSpriteType::End)>, RailSpriteType> group{};
+	EnumClassArray<const SpriteGroup *, RailSpriteType, RailSpriteType::End> group{};
 
 	std::vector<BadgeID> badges;
 

src/station_cmd.cpp
@@ -2451,8 +2451,8 @@ static CommandCost RemoveGenericRoadStop(DoCommandFlags flags, const TileArea &r
 
 		/* Save information on to-be-restored roads before the stop is removed. */
 		RoadBits road_bits{};
-		EnumClassIndexContainer<std::array<RoadType, to_underlying(RoadTramType::End)>, RoadTramType> road_type{INVALID_ROADTYPE, INVALID_ROADTYPE};
-		EnumClassIndexContainer<std::array<Owner, to_underlying(RoadTramType::End)>, RoadTramType> road_owner{OWNER_NONE, OWNER_NONE};
+		EnumClassArray<RoadType, RoadTramType, RoadTramType::End> road_type{INVALID_ROADTYPE, INVALID_ROADTYPE};
+		EnumClassArray<Owner, RoadTramType, RoadTramType::End> road_owner{OWNER_NONE, OWNER_NONE};
 		if (IsDriveThroughStopTile(cur_tile)) {
 			for (RoadTramType rtt : ROADTRAMTYPES_ALL) {
 				road_type[rtt] = GetRoadType(cur_tile, rtt);

src/table/newgrf_debug_data.h
@@ -721,7 +721,7 @@ static const NIFeature _nif_roadstop = {
 };
 
 /** Table with all NIFeatures. */
-static const EnumClassIndexContainer<std::array<const NIFeature *, to_underlying(GrfSpecFeature::FakeEnd)>, GrfSpecFeature> _nifeatures{
+static const EnumClassArray<const NIFeature *, GrfSpecFeature, GrfSpecFeature::FakeEnd> _nifeatures{
 	&_nif_vehicle, // GrfSpecFeature::Trains
 	&_nif_vehicle, // GrfSpecFeature::RoadVehicles
 	&_nif_vehicle, // GrfSpecFeature::Ships

src/table/pricebase.h
@@ -7,7 +7,7 @@
 
 /** @file pricebase.h Table of all default price bases. */
 
-static const EnumClassIndexContainer<std::array<PriceBaseSpec, to_underlying(Price::End)>, Price> _price_base_specs = {
+static const EnumClassArray<PriceBaseSpec, Price, Price::End> _price_base_specs = {
 	PriceBaseSpec(100, PCAT_NONE, GrfSpecFeature::End, Price::Invalid), // Price::StationValue
 	PriceBaseSpec(100, PCAT_CONSTRUCTION, GrfSpecFeature::End, Price::Invalid), // Price::BuildRail
 	PriceBaseSpec(95, PCAT_CONSTRUCTION, GrfSpecFeature::End, Price::Invalid), // Price::BuildRoad

src/tar_type.h
@@ -21,7 +21,7 @@ struct TarFileListEntry {
 
 using TarList = std::map<std::string, std::string, std::less<>>; ///< Map of tar file to tar directory.
 using TarFileList = std::map<std::string, TarFileListEntry, std::less<>> ;
-extern EnumClassIndexContainer<std::array<TarList, to_underlying(Subdirectory::End)>, Subdirectory> _tar_list;
-extern EnumClassIndexContainer<std::array<TarFileList, to_underlying(Subdirectory::End)>, Subdirectory> _tar_filelist;
+extern EnumClassArray<TarList, Subdirectory, Subdirectory::End> _tar_list;
+extern EnumClassArray<TarFileList, Subdirectory, Subdirectory::End> _tar_filelist;
 
 #endif /* TAR_TYPE_H */

JGRennison/Upstream-OpenTTD

src/company_cmd.cpp
@@ -470,7 +470,7 @@ bad_town_name:;
 }
 
 /** Sorting weights for the company colours. */
-static const EnumClassIndexContainer<std::array<uint8_t, to_underlying(Colours::End)>, Colours> _colour_sort{2, 2, 3, 2, 3, 2, 3, 2, 3, 2, 2, 2, 3, 1, 1, 1};
+static const EnumIndexArray<uint8_t, Colours, Colours::End> _colour_sort{2, 2, 3, 2, 3, 2, 3, 2, 3, 2, 2, 2, 3, 1, 1, 1};
 /** Similar colours, so we can try to prevent same coloured companies. */
 static const std::initializer_list<Colours> _similar_colour[to_underlying(Colours::End)] = {
 	{Colours::Blue, Colours::LightBlue }, // Colours::DarkBlue

src/fontcache.h
@@ -22,7 +22,7 @@ static const GlyphID SPRITE_GLYPH = 1U << 30;
 /** Font cache for basic fonts. */
 class FontCache {
 protected:
-	static EnumClassIndexContainer<std::array<std::unique_ptr<FontCache>, to_underlying(FontSize::End)>, FontSize> caches; ///< All the font caches.
+	static EnumIndexArray<std::unique_ptr<FontCache>, FontSize, FontSize::End> caches; ///< All the font caches.
 	std::unique_ptr<FontCache> parent; ///< The parent of this font cache.
 	const FontSize fs; ///< The size of the font.
 	int height = 0; ///< The height of the font.
@@ -42,9 +42,9 @@ public:
 	static void ClearFontCaches(FontSizes fontsizes);
 
 	/** Default unscaled font heights. */
-	static const EnumClassIndexContainer<std::array<int, to_underlying(FontSize::End)>, FontSize> DEFAULT_FONT_HEIGHT;
+	static const EnumIndexArray<int, FontSize, FontSize::End> DEFAULT_FONT_HEIGHT;
 	/** Default unscaled font ascenders. */
-	static const EnumClassIndexContainer<std::array<int, to_underlying(FontSize::End)>, FontSize> DEFAULT_FONT_ASCENDER;
+	static const EnumIndexArray<int, FontSize, FontSize::End> DEFAULT_FONT_ASCENDER;
 
 	static int GetDefaultFontHeight(FontSize fs);
 

src/fontcache/spritefontcache.cpp
@@ -33,7 +33,7 @@ static int ScaleFontTrad(int value)
 	return UnScaleByZoom(value * ZOOM_BASE, _font_zoom);
 }
 
-static EnumClassIndexContainer<std::array<std::unordered_map<char32_t, SpriteID>, to_underlying(FontSize::End)>, FontSize> _char_maps{}; ///< Glyph map for each font size.
+static EnumIndexArray<std::unordered_map<char32_t, SpriteID>, FontSize, FontSize::End> _char_maps{}; ///< Glyph map for each font size.
 
 /**
  * Get SpriteID associated with a character.

src/rail.h
@@ -262,12 +262,12 @@ public:
 	/**
 	 * NewGRF providing the Action3 for the railtype. nullptr if not available.
 	 */
-	EnumClassIndexContainer<std::array<const GRFFile *, to_underlying(RailSpriteType::End)>, RailSpriteType> grffile{};
+	EnumIndexArray<const GRFFile *, RailSpriteType, RailSpriteType::End> grffile{};
 
 	/**
 	 * Sprite groups for resolving sprites
 	 */
-	EnumClassIndexContainer<std::array<const SpriteGroup *, to_underlying(RailSpriteType::End)>, RailSpriteType> group{};
+	EnumIndexArray<const SpriteGroup *, RailSpriteType, RailSpriteType::End> group{};
 
 	std::vector<BadgeID> badges;
 

src/station_cmd.cpp
@@ -2451,8 +2451,8 @@ static CommandCost RemoveGenericRoadStop(DoCommandFlags flags, const TileArea &r
 
 		/* Save information on to-be-restored roads before the stop is removed. */
 		RoadBits road_bits{};
-		EnumClassIndexContainer<std::array<RoadType, to_underlying(RoadTramType::End)>, RoadTramType> road_type{INVALID_ROADTYPE, INVALID_ROADTYPE};
-		EnumClassIndexContainer<std::array<Owner, to_underlying(RoadTramType::End)>, RoadTramType> road_owner{OWNER_NONE, OWNER_NONE};
+		EnumIndexArray<RoadType, RoadTramType, RoadTramType::End> road_type{INVALID_ROADTYPE, INVALID_ROADTYPE};
+		EnumIndexArray<Owner, RoadTramType, RoadTramType::End> road_owner{OWNER_NONE, OWNER_NONE};
 		if (IsDriveThroughStopTile(cur_tile)) {
 			for (RoadTramType rtt : ROADTRAMTYPES_ALL) {
 				road_type[rtt] = GetRoadType(cur_tile, rtt);

src/table/newgrf_debug_data.h
@@ -721,7 +721,7 @@ static const NIFeature _nif_roadstop = {
 };
 
 /** Table with all NIFeatures. */
-static const EnumClassIndexContainer<std::array<const NIFeature *, to_underlying(GrfSpecFeature::FakeEnd)>, GrfSpecFeature> _nifeatures{
+static const EnumIndexArray<const NIFeature *, GrfSpecFeature, GrfSpecFeature::FakeEnd> _nifeatures{
 	&_nif_vehicle, // GrfSpecFeature::Trains
 	&_nif_vehicle, // GrfSpecFeature::RoadVehicles
 	&_nif_vehicle, // GrfSpecFeature::Ships

src/table/pricebase.h
@@ -7,7 +7,7 @@
 
 /** @file pricebase.h Table of all default price bases. */
 
-static const EnumClassIndexContainer<std::array<PriceBaseSpec, to_underlying(Price::End)>, Price> _price_base_specs = {
+static const EnumIndexArray<PriceBaseSpec, Price, Price::End> _price_base_specs = {
 	PriceBaseSpec(100, PCAT_NONE, GrfSpecFeature::End, Price::Invalid), // Price::StationValue
 	PriceBaseSpec(100, PCAT_CONSTRUCTION, GrfSpecFeature::End, Price::Invalid), // Price::BuildRail
 	PriceBaseSpec(95, PCAT_CONSTRUCTION, GrfSpecFeature::End, Price::Invalid), // Price::BuildRoad

src/tar_type.h
@@ -21,7 +21,7 @@ struct TarFileListEntry {
 
 using TarList = std::map<std::string, std::string, std::less<>>; ///< Map of tar file to tar directory.
 using TarFileList = std::map<std::string, TarFileListEntry, std::less<>> ;
-extern EnumClassIndexContainer<std::array<TarList, to_underlying(Subdirectory::End)>, Subdirectory> _tar_list;
-extern EnumClassIndexContainer<std::array<TarFileList, to_underlying(Subdirectory::End)>, Subdirectory> _tar_filelist;
+extern EnumIndexArray<TarList, Subdirectory, Subdirectory::End> _tar_list;
+extern EnumIndexArray<TarFileList, Subdirectory, Subdirectory::End> _tar_filelist;
 
 #endif /* TAR_TYPE_H */